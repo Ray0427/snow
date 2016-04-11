d65dcd9d5b24fd4107a81a07d28f7ee7fdaf2dcc
RAY <ray13242000@gmail.com>
2016-04-11 14:43:08 +0800

    gtm

--- js/flip.js
+++ js/flip.js
@@ -1,4 +1,50 @@
+// function flip(e) {
+//   var front = document.getElementsByClassName('front')[0];
+//   var back = document.getElementsByClassName('back')[0];
+//   // var current=document.getElementsByClassName('current')[0];
+//   switch (e) {
+//     case 1:
+//       front.classList.add('flipOutY');
+//       setTimeout(function() {
+//         // front.style.zIndex = 0;
+//         // back.style.zIndex = 1;
+//         // front.classList.add('out');
+//         front.classList.remove('flipOutY');
+//         // front.classList.add('hide');
+//         // var frontImg=front.getElementsByTagName('img');
+//         // for (var i = 0; i < frontImg.length; i++) {
+//         // 	frontImg[i].classList.add('hide');
+//         // }
+//       }, 3000)
+//       break;
+//     case 2:
+//       front.classList.add('flipInY');
+//       setTimeout(function() {
+//         // front.style.zIndex = 0;
+//         // back.style.zIndex = 1;
+//         // front.classList.remove('out');
+//         // front.classList.remove('flip-back');
 
-function flip(e) {
-  document.querySelector('.front').className = document.querySelector('.front').className + " flip";
+//       }, 3000)
+//       break;
+//   }
+
+
+// }
+function e(){
+document.getElementById('card').classList.add('flipped');
+//   var front = document.getElementsByClassName('front')[0];
+//   var back = document.getElementsByClassName('back')[0];
+//   front.classList.add('flip');
+//     // back.classList.add('flip');
+// setTimeout(function() {
+
+//         front.classList.add('flipped');
+// //         front.classList.remove('flipOutY');
+//         back.classList.add('flipped');
+// //         // var frontImg=front.getElementsByTagName('img');
+// //         // for (var i = 0; i < frontImg.length; i++) {
+// //         //   frontImg[i].classList.add('hide');
+// //         // }
+//       }, 3000)
 }
